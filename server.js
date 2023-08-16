b311b49dfb0fb898331cef06c5cb4be7e0c00508
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-08-17 00:45:32 +0900

    MongoDB get post 데이터 바인딩

--- server.js
+++ server.js
@@ -1,35 +1,29 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
-// Post 요청으로 서버에 데이터 전송시 필요!
-
+app.use(bodyParser.urlencoded({extended: true})); // Post 요청으로 서버에 데이터 전송시 필요!
 const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
+
 
 const url = 'mongodb+srv://starirene9:gzKhvuSABTyfrIus@cluster0.cexbyak.mongodb.net/todoapp?retryWrites=true&w=majority';
 
-MongoClient.connect(url, {useUnifiedTopology: true}, (error, client) => {
+var db;
+MongoClient.connect(url, {useUnifiedTopology: true}, function (error, client) {
     // database 접속이 완료되면 할 일
-    if (error) return console.log(error)
+    if (error) return console.log(error);
+    db = client.db('todoapp'); // todoapp이라는 Database 에 연결해 줌
 
-    const db = client.db('todoapp'); // todoapp이라는 Database 에 연결해 줌
+    // db.collection('post').insertOne( 저장할 데이터 {}, function(에러, 결과)형태 기억! 데이터 저장 단계
+    db.collection('post').insertOne({이름: 'Bitna', _id: 100}, function (insertError, result) {
+        console.log('저장완료');
+    });
 
-    // db.collection('post').insertOne() 이 형태 기억할 것~!
-    db.collection('post').insertOne({이름: 'Bitna', _id : 100}, (insertError, result) => {
-        if (insertError) {
-            console.error('저장 실패:', insertError);
-        } else {
-            console.log('저장 완료', result.insertedId);
-        }
-
-        app.listen(8080, () => { // 포트 번호 8080, 열리면 하는 기능 적기
-            console.log('listening on 8080') // localhost:8080 에 서버 만든거임
-        });
+    app.listen(8080, function () { // 포트 번호 8080, 열리면 하는 기능 적기
+        console.log('listening on 8080') // localhost:8080 에 서버 만든거임
     });
 });
 
-
-// 위는 서버 오픈하는 기본 세가지 문법
 
 // 서버에 get 요청하기
 // app.get('/pet', function(request, response){
@@ -53,11 +47,22 @@ app.get('/checklist', function (request, response) {
     response.sendFile(__dirname + '/checklist.html')
 }); // 이것이 API 이다.
 
-// 어떤 사람이 /add 경로로 post 요청을 하면 ?? 을 해주세요.
-app.post('/add', function (request, response) {
-    console.log(request.body);
-    response.send('전송완료')
-//  DB에 저장해주세요~! REST API
+// 1. 어떤 사람이 /add 라는 경로로 post 요청을 하면, 2. 데이터 2개 (날짜(date), 제목(title)을 보내주는데,
+// 이때 'post'라는 이름을 가진 collection에 두개 데이터를 저장하기
+app.post('/add', function (request, response) {  // 1.
+    response.send('전송완료');
+    console.log(request.body.title);
+    console.log(request.body.date); // 2
+
+    db.collection('post').insertOne({제목: request.body.title, 날짜: request.body.date}, function (error, result) {
+        console.log('저장완료');
+    });
+});
+
+//list 로 GET 요청으로 접속하면 HTML을 보여줌, 실제 DB에 저장된 데이터들로 예쁘게 꾸며진 HTML 보여주기
+
+app.get('/list', function (request, response) {
+    response.render('list.ejs'); // 외워서 쓸것! 누군가 /list 로 접속하면 list.ejs 파일을 보여 줌
 });
 
 
